29b680d650b2066ecfe2c0c15bec18b570699b02
kirill <kirill@Mac-mini-kirill.local>
2019-12-18 14:18:54 +0200

    8.mp4

--- routes/todo.js
+++ routes/todo.js
@@ -1,24 +1,58 @@
 const {Router} = require('express')
+const Todo = require('../models/todo')
 const router = Router()
 
 // Geet ToDo List
 router.get('/', (req, res) => {
   res.json({a: 1})
+  try{
+    
+  } catch(err) {
+    console.log(err)
+    res.status(500).json({
+      message: 'Server error'
+    })
+  }
 })
 
 // Create new ToDo
-router.post('/', (req, res) => {
-
+router.post('/', async (req, res) => {
+  try{
+    const todo = await Todo.create({
+      title: req.body.title,
+      done: false
+    })
+    res.status(201).json({todo})
+  } catch(err) {
+    console.log(err)
+    res.status(500).json({
+      message: 'Server error'
+    })
+  }
 })
 
 // Change Some Task
 router.put('/:id', (req, res) => {
-  
+  try{
+    
+  } catch(err) {
+    console.log(err)
+    res.status(500).json({
+      message: 'Server error'
+    })
+  }
 })
 
 // Delete Some Task
 router.delete('/:id', (req, res) => {
-  
+  try{
+    
+  } catch(err) {
+    console.log(err)
+    res.status(500).json({
+      message: 'Server error'
+    })
+  }
 })
 
 module.exports = router
