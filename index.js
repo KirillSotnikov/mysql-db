012d0f33acd04e24dbd5e0d61fb5520fb2f56e60
kirill <kirill@Mac-mini-kirill.local>
2019-12-18 13:52:25 +0200

    5.mp4

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const seqelize = require('./utils/database')
 const todoRoutes = require('./routes/todo')
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -13,4 +14,14 @@ app.use((req, res, next) => {
   res.sendFile('/index.html')
 })
 
-app.listen(PORT)
+async function start() {
+  try{
+    await seqelize.sync()
+
+    app.listen(PORT)
+  } catch( err ){
+    console.log(err)
+  }
+}
+
+start()
